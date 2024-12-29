42b2dc357bfdff38f438c3c2293546c2d934a9f1
SID <managebydev@gmail.com>
2024-12-29 22:33:53 +0500

    project created

---
+++ swiftDataWorking/swiftDataWorkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftDataWorkingApp.swift
+//  swiftDataWorking
+//
+//  Created by SID on 29/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftDataWorkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
